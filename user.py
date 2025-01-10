f54021d414b8e3f8258b1bd34e3ffa0df82006cc
RikhardHonkanen <rikhard@raket.co>
2025-01-10 22:50:15 +0100

    user functions

--- user.py
+++ user.py
@@ -2,6 +2,7 @@ import os
 import helpers
 
 USER_DATA_PATH = "data/output"
+# Read
 def set():
     return helpers.parse_file(f"{USER_DATA_PATH}/set.txt")
 
@@ -10,3 +11,16 @@ def unique():
 
 def other():
     return helpers.parse_file(f"{USER_DATA_PATH}/other.txt")
+
+# Delete
+def del_set(set_item):
+    # implement delete item
+    return f"deleted {set_item}"
+
+def del_unique(unique_item):
+    # implement delete item
+    return f"deleted {unique_item}"
+
+def del_other(other_item):
+    # implement delete item
+    return f"deleted {other_item}"
