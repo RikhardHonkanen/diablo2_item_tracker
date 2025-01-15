0a618601c7050eb38eaee046be552cb4e79da8de
RikhardHonkanen <rikhard@raket.co>
2025-01-16 00:55:20 +0100

    init unique items

--- master.py
+++ master.py
@@ -8,7 +8,7 @@ class MasterData:
     def __init__(self):
         # Initialize data by reading files
         self.set_items = init_master_set_items()
-        self.unique_items = helpers.parse_file(f"{MASTER_DATA_PATH}/unique.txt")
+        self.unique_items = init_master_unique_items()
         # self.other_items = helpers.parse_file(f"{MASTER_DATA_PATH}/other.txt")
         
 def init_master_set_items():
@@ -35,8 +35,20 @@ def init_master_set_items():
     return set_items
 
 def init_master_unique_items():
+    """Initialize master unique items, same handling as for set items"""
     unique_items_raw = helpers.parse_file(f"{MASTER_DATA_PATH}/unique.txt")
     unique_items = {}
+    labels = unique_items_raw.pop(0).split('\t')
+    
+    for idx, item in enumerate(unique_items_raw):
+        entries = item.split('\t')
+        unique_items[entries[0]] = {}
+        for idx, label in enumerate(labels):
+            # If the index is out of range or the value is an empty string, assign "N/A"
+            if idx >= len(entries) or entries[idx].strip() == "":
+                unique_items[entries[0]][label] = "N/A"
+            else:
+                unique_items[entries[0]][label] = entries[idx]
     
     return unique_items
     
